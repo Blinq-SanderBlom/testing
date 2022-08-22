855750972e48b783e3556e29e8248e62cbc3025f
Sander Blomvågnes <sander@blinq.n>
2022-08-22 10:47:07 +0200

    Added route to test

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/test', (req, res) => {
+  res.send('Testing testing')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
